65fcfee6ae614a93aadb5501add4e88fbdffae61
Adeyinka Giwa <yinkadev1992@gmail.com>
2021-12-31 16:35:16 +0000

    using google api to get coordinates

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div class="container">
+      <h1>Please enter a Postcode</h1>
+      <input id="postcode"/>
+      <button @click="geocoder">Submit</button>
+    </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  created() {
+    console.log("started")
+  },
+  methods: {
+    geocoder() {
+      axios.post('https://maps.googleapis.com/maps/api/geocode/json?address=mk429jz&key=AIzaSyA8DwLkDo-ZqT5h7nsq2MaZPQAvJbv48vY')
+      .then(res=>console.log(res))
     }
   }
 }
+</script>
+
+<style lang="scss" scoped>
+  .container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h1{
+      margin-bottom: 2.5rem;
+    }
+    input{
+      margin-bottom: 1rem;
+    }
+    button{
+      &:hover{
+        cursor: pointer;
+        background-color: darkgray;
+      }
+    }
+  }
 </style>
