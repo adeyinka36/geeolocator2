4d6d25e65a25d10253fb0eb8b627d5d60335f179
Adeyinka Giwa <yinkadev1992@gmail.com>
2022-02-12 09:42:26 +0000

    fixing wrong repo name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git master:geolocator2
+git push -f  git@github.com:adeyinka36/geeolocator2.git master:geeolocator2
 cd -
